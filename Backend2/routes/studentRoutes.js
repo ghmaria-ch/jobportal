2079be4cb5502beea2a4ef13fc5381fe053340c6
maria christo <maria.phone.2020@gmail.com>
2025-10-10 03:45:35 +0530

    updated student routes

--- Backend2/routes/studentRoutes.js
+++ Backend2/routes/studentRoutes.js
@@ -1,14 +1,35 @@
+// // const express = require("express");
+
+// // const { getprofile ,addprofile,editprofile,getallprofiles,updatestudentrating} = require("../controllers/studentController"); // Import signup logic
+// // const router = express.Router();
+
+
+
+// // router.get("/getprofile/:studentId",getprofile);
+// // router.get("/getallprofiles",getallprofiles)
+// // router.post("/addprofile",addprofile);
+// // router.put("/editprofile/:studentId",editprofile);
+// // router.put("/updatestudentrating/:studentId",updatestudentrating);
+// // module.exports = router;
+
+
 // const express = require("express");
 
-// const { getprofile ,addprofile,editprofile,getallprofiles,updatestudentrating} = require("../controllers/studentController"); // Import signup logic
+// const { 
+//     getprofile ,
+//     addprofile,
+//     editprofile,
+//     getallprofiles,
+//     updatestudentrating,
+//     uploadCertificates // 📌 IMPORT NEW MIDDLEWARE
+// } = require("../controllers/studentController"); 
+
 // const router = express.Router();
-
-
 
 // router.get("/getprofile/:studentId",getprofile);
 // router.get("/getallprofiles",getallprofiles)
-// router.post("/addprofile",addprofile);
-// router.put("/editprofile/:studentId",editprofile);
+// router.post("/addprofile", uploadCertificates, addprofile); // 📌 APPLY MIDDLEWARE
+// router.put("/editprofile/:studentId", uploadCertificates, editprofile); // 📌 APPLY MIDDLEWARE
 // router.put("/updatestudentrating/:studentId",updatestudentrating);
 // module.exports = router;
 
@@ -21,16 +42,14 @@ const {
     editprofile,
     getallprofiles,
     updatestudentrating,
-    uploadCertificates // 📌 IMPORT NEW MIDDLEWARE
+    uploadCertificates // 📌 IMPORTED MIDDLEWARE
 } = require("../controllers/studentController"); 
 
 const router = express.Router();
 
 router.get("/getprofile/:studentId",getprofile);
 router.get("/getallprofiles",getallprofiles)
-router.post("/addprofile", uploadCertificates, addprofile); // 📌 APPLY MIDDLEWARE
-router.put("/editprofile/:studentId", uploadCertificates, editprofile); // 📌 APPLY MIDDLEWARE
+router.post("/addprofile", uploadCertificates, addprofile); // 📌 APPLIED
+router.put("/editprofile/:studentId", uploadCertificates, editprofile); // 📌 APPLIED
 router.put("/updatestudentrating/:studentId",updatestudentrating);
 module.exports = router;
-
-
